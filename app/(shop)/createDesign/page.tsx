b3970876e7692e796310998a78782923fe376364
bazraatamir <lhagvasuren.bazarragchaa89@gmail.com>
2025-06-04 17:26:03 +0800

    fix

--- app/(shop)/createDesign/page.tsx
+++ app/(shop)/createDesign/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import {Canvas} from "@react-three/fiber";
+import {OrbitControls} from "@react-three/drei";
 import Polo from "../_components/Polo";
 
 export default function Page() {
@@ -9,7 +9,7 @@ export default function Page() {
     <Canvas>
       <ambientLight intensity={0.5} />
       <directionalLight position={[10, 10, 5]} intensity={1} />
-      <Polo model="/models/Polo.gltf" />
+      <Polo model='/models/Polo.gltf' />
       <OrbitControls enableZoom={false} />
     </Canvas>
   );
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -7,45 +7,50 @@ import {
 } from "@/components/ui/navigation-menu";
 
 import Link from "next/link";
-import { SearchForm } from "./search-form";
-import { ShoppingCart, User } from "lucide-react";
+import {SearchForm} from "./search-form";
+import {ShoppingCart, User} from "lucide-react";
 import logo from "../public/logo.png";
 import Image from "next/image";
-import { ModeToggle } from "./theme-changer";
+import {ModeToggle} from "./theme-changer";
+
 export default function Navbar() {
   return (
-    <div className="flex items-center justify-between flex-row  border-b-2 w-[94%] ml-[3%] p-3 ">
-      <Link href="/" className="flex items-center gap-4">
-        <Image src={logo} alt="Khiilen logo" width={125} />
+    <div className='flex items-center justify-between flex-row border-b-2 w-[94%] ml-[3%] p-3'>
+      <Link href='/' className='flex items-center gap-4'>
+        <Image src={logo} alt='Khiilen logo' width={125} />
       </Link>
+
       <NavigationMenu>
         <NavigationMenuList>
           <NavigationMenuItem>
-            <Link href="/collections">
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Бараа
-              </NavigationMenuLink>
-            </Link>
+            <NavigationMenuLink
+              asChild
+              className={navigationMenuTriggerStyle()}>
+              <Link href='/collections'>Бараа</Link>
+            </NavigationMenuLink>
           </NavigationMenuItem>
+
           <NavigationMenuItem>
-            <Link href="/docs">
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Захиалга
-              </NavigationMenuLink>
-            </Link>
+            <NavigationMenuLink
+              asChild
+              className={navigationMenuTriggerStyle()}>
+              <Link href='/docs'>Захиалга</Link>
+            </NavigationMenuLink>
           </NavigationMenuItem>
+
           <NavigationMenuItem>
-            <Link href="/createDesign">
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Загвар үүсгэх
-              </NavigationMenuLink>
-            </Link>
+            <NavigationMenuLink
+              asChild
+              className={navigationMenuTriggerStyle()}>
+              <Link href='/createDesign'>Загвар үүсгэх</Link>
+            </NavigationMenuLink>
           </NavigationMenuItem>
         </NavigationMenuList>
       </NavigationMenu>
-      <div className="flex items-center gap-4 flex-row ">
+
+      <div className='flex items-center gap-4 flex-row'>
         <SearchForm />
-        <Link href="/cart">
+        <Link href='/cart'>
           <ShoppingCart />
         </Link>
         <User />
